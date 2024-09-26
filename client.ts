207ba951d4770782cdbcd06c84bf3b338fb57c22
tonynazarov <tonynazarov@gmail.com>
2024-09-26 17:45:07 +1200

    move to cli app + kafka

--- client.ts
+++
@@ -1,9 +0,0 @@
-import {MetlinkHttpClientBuilder} from "metlink-api-http-client";
-import * as fs from 'fs'
-
-export function getHttpClient() {
-    const token: string = fs.readFileSync(".token", 'utf-8');
-
-    return MetlinkHttpClientBuilder.buildWithAxios(token)
-}
-
